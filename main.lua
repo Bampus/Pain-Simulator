aa5c0b92595b708fe5f0266fa317a6b15dd8e95a
Bampus <65647017+Bampus@users.noreply.github.com>
2021-02-21 19:21:16 +0100

    rörelse

--- main.lua
+++ main.lua
@@ -1,4 +1,19 @@
-function love.draw()
-love.graphics.setColor(1, 0, 0)
-love.graphics.circle("fill", 400, 550, 50, 800)
-end
+
+local sphere = {
+    x  = 400,
+    y  = 300,
+    vx = 0,
+    vy = -100,
+ }
+ 
+
+ function love.update(dt)
+   
+    sphere.x = sphere.x + (sphere.vx * dt) 
+    sphere.y = sphere.y + (sphere.vy * dt)
+ end
+ 
+ 
+ function love.draw()
+    love.graphics.circle("fill", sphere.x, sphere.y, 50) 
+ end
