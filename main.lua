ee5e65af28cae76ea6e33defe9a3a075370a7c2b
Bampus <65647017+Bampus@users.noreply.github.com>
2021-04-09 14:17:04 +0200

    steg_1_kollision

--- main.lua
+++ main.lua
@@ -1,21 +1,44 @@
+bump = require "bump"
+world = bump.newWorld()
 
 player = {
   x = 0,
   y = 0,
   width = 32,
   height = 32,
-  gravity = 400,
+  gravity = 500,
   runSpeed = 600,
   xVelocity = 0,
   yVelocity = 0,
-  terminalVelocity = 500
+  terminalVelocity = 600
 }
+
+local block = {
+  x = -1,
+  y = 551,
+  width = 1200,
+  height = 50
+}
+
+function love.update(dt)
+  player.update(dt)
+end
 
 function player.update(dt)
   player.move(dt)
   player.applyGravity(dt)
-  player.x = player.x + player.xVelocity * dt 
-  player.y = player.y + player.yVelocity * dt
+  player.collide(dt)
+end
+
+function player.setPosition(x, y)
+  player.x = x
+  player.y = y
+end
+
+function love.load()
+  player.setPosition(love.graphics.getWidth()/2, 540)
+  world:add(player, player.x, player.y, player.width, player.height)
+  world:add(block, block.x, block.y, block.width, block.height)
 end
 
 function player.move(dt)
@@ -36,24 +59,28 @@ function player.applyGravity(dt)
   end
 end
 
+function player.collide(dt)
+  local futureX = player.x + player.xVelocity * dt
+  local futureY = player.y + player.yVelocity * dt
+  local nextX, nextY, cols, len = world:move(player, futureX, futureY)
+  player.x = nextX
+  player.y = nextY
+  
+  for i = 1, len do
+    local col = cols[i]
+    if col.normal.x == -1 or col.normal.y == 1 then
+      player.yVelocity = 0
+    end
+  end
+
+end
+
 function player.draw()
   love.graphics.setColor(1, 1, 1)
   love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
 end 
 
-function player.setPosition(x, y)
-  player.x = x
-  player.y = y
-end
-
-function love.load()
-  player.setPosition(love.graphics.getWidth()/2, 0)
-end
-
-function love.update(dt)
-  player.update(dt)
-end
-
 function love.draw()
   player.draw()
+  love.graphics.rectangle("line", block.x, block.y, block.width, block.height)
 end
